4df5293544439f4572cc4678d3dd9645f54e373c
Monnylly <monnylly@gmail.com>
2021-09-29 19:21:28 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import About from '../components/About';
+
+const imagem = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+describe('testando arquivo About', () => {
+  test('se contém um heading "h2" com o texto "About Pokédex"', () => {
+    render(<About />);
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('se contem dois paragrafos com o texto sobre a Pokédex', () => {
+    render(<About />);
+    const paragrafo1 = screen.getByText(/This application simulates a Pokédex/i);
+    const paragrafo2 = screen.getByText(/One can filter Pokémons by type/i);
+
+    expect(paragrafo1).toBeInTheDocument();
+    expect(paragrafo2).toBeInTheDocument();
+  });
+
+  test('Verifica se imagem de uma Pokédex aparece na tela', () => {
+    render(<About />);
+    const imagPokedex = screen.getByRole('img');
+
+    expect(imagPokedex.src).toBe(imagem);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/RenderWith';
 
-dss
+import App from '../App';
+// import userEvent from '@testing-library/user-event';
+
+describe('testando componente App', () => {
+  test('testando a Home', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', {
+      name: 'Home',
+    });
+
+    expect(home).toBeInTheDocument();
+
+    const url = 
+    userEvent.onclick(home).toBe('/');
+  });
+
+  // const home = screen.getByRole('link', {
+  //   nome: 'Home',
+  // });
+});
+
+// test('primeiro link deve possuir o texto "Home"', () => {
+//   const oneLink = screen.getByRole('/Home');
+//   const linkHomeText = screen.getByText()
+//   expect(getByRole())
+
+// });
