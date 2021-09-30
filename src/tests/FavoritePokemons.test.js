45f0ec3064bf58ac490d06b42726f728c09fa429
Monnylly <monnylly@gmail.com>
2021-09-30 15:57:36 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+describe('Favorites test', () => {
+  test('verifica se existe o texto "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notText = screen.getByText(/No favorite pokemon found/i);
+    expect(notText).toBeInTheDocument();
+  });
+
+  test('Verifica se é exibido todos os cards de pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+    const cardPokemon = screen.getByTestId('pokemon-name');
+    expect(cardPokemon).toBeInTheDocument();
+  });
+});
