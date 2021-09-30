f93feedb28aa960914dce4a88f413e1d51f57ccb
Monnylly <monnylly@gmail.com>
2021-09-30 08:48:53 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,32 +1,53 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../components/RenderWith';
+// import renderWithRouter from '../components/RenderWith';
+import renderWithRouter from '../utils/renderWithRouter';
 
 import App from '../App';
-// import userEvent from '@testing-library/user-event';
 
 describe('testando componente App', () => {
-  test('testando a Home', () => {
+  test('testando se existe um link com o texto "Home"', () => {
     renderWithRouter(<App />);
     const home = screen.getByRole('link', {
       name: 'Home',
     });
 
     expect(home).toBeInTheDocument();
-
-    const url = 
-    userEvent.onclick(home).toBe('/');
   });
 
-  // const home = screen.getByRole('link', {
-  //   nome: 'Home',
-  // });
+  test('testando se existe um link com o texto "About"', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    expect(about).toBeInTheDocument();
+  });
+
+  test('testando se existe um link com o texto "Favorites PoKémons"', () => {
+    renderWithRouter(<App />);
+    const favorites = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favorites).toBeInTheDocument();
+  });
+  test('verifica se a Home esta sendo direcionada para "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Home'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('verifica se About esta sendo direcionada para "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  test('verifica se Favorites esta sendo direcionada para "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });
-
-// test('primeiro link deve possuir o texto "Home"', () => {
-//   const oneLink = screen.getByRole('/Home');
-//   const linkHomeText = screen.getByText()
-//   expect(getByRole())
-
-// });
